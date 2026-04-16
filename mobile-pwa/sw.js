75d725caddb5ce645b728af61c9c26c533840d43
Yaniv Mizrachi <yanivmiz77@gmail.com>
2026-04-16 16:34:52 +0300

    Bump PWA cache version

--- mobile-pwa/sw.js
+++ mobile-pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE='model2-mobile-pwa-v1';
+const CACHE='model2-mobile-pwa-v1776346491';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
